1896cc059a207798c19c650c10d10a02add64257
Benson Jing <bensonjing@icloud.com>
2022-08-29 19:30:52 -0400

    implement skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,38 @@
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div id="skills">
+      <h2>Skills</h2>
+      <ul id="languages">
+        Languages
+        <li>Python</li>
+        <li>C++</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Java</li>
+      </ul>
+      <ul id="technologies">
+        Technologies
+        <li>ReactJS</li>
+        <li>NodeJS</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>Mongoose</li>
+        <li>PassportJS</li>
+        <li>Firebase</li>
+      </ul>
+      <ul id="tools">
+        Tools
+        <li>Git/GitHub</li>
+        <li>npm</li>
+        <li>Webpack</li>
+        <li>Heroku</li>
+        <li>VSCode</li>
+        <li>Vim/Neovim</li>
+        <li>Terminal/Zsh</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Skills;
