638c65cbe8483b1b0f2f5a00864cb435e1e4f4b2
Benson Jing <bensonjing@icloud.com>
2022-08-29 15:52:18 -0400

    implement header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Intro />
       <Skills />
       <Projects />
-      <Footer />
+      <Contact />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,20 @@
 function Header() {
-  return <div>Header</div>;
+  const goToContact = (e) => {
+    e.preventDefault();
+    window.location.replace("/#contact");
+  };
+
+  return (
+    <div id="header">
+      <div>BJ</div>
+      <div>
+        <a href="https://github.com/bensonjing" target="_blank">
+          GitHub
+        </a>
+        <div onClick={(e) => goToContact(e)}>Contact Me</div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
