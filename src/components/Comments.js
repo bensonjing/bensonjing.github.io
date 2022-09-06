a3742edada94a2968b725e1ebcdae042b3a39f88
Benson Jing <bensonjing@icloud.com>
2022-09-05 20:26:32 -0400

    implement post detail

---
+++ src/components/Comments.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const API_URL = "https://benson-blog-api.herokuapp.com";
+
+function Comments({ postId }) {
+  const [comments, setComments] = useState([]);
+  const [username, setUsername] = useState("");
+  const [content, setContent] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  const getComments = () => {
+    fetch(`${API_URL}/api/posts/${postId}/comments`)
+      .then((response) => response.json())
+      .then((json) => setComments(json));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${API_URL}/api/posts/${postId}/comments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          content: content,
+        }),
+      });
+
+      if (response.status === 200) {
+        setUsername("");
+        setContent("");
+        setMessage("Comment created successfully");
+        getComments();
+      } else {
+        setMessage("Some error occurred");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="comments">
+      <div className="title">Comments</div>
+
+      <form onSubmit={handleSubmit} id="new_comment">
+        <div className="title">New Comment</div>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Name:"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <textarea
+          type="text"
+          id="content"
+          name="content"
+          placeholder="Comment:"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
+        />
+        <button>Submit</button>
+
+        <div>{message ? <p>{message}</p> : null}</div>
+      </form>
+
+      {comments.map((comment) => (
+        <div className="comment" key={uuidv4()}>
+          <div className="title">{comment.username}</div>
+          <div>{comment.content}</div>
+          <div>{new Date(comment.createdAt).toLocaleString()}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Comments;
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,5 +1,30 @@
-function PostDetail({ id }) {
-  return <div>id</div>;
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Comments from "./Comments";
+
+function PostDetail() {
+  const { postId } = useParams();
+  const [post, setPost] = useState({ author: {} });
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  const getPost = () => {
+    fetch(`https://benson-blog-api.herokuapp.com/api/posts/${postId}`)
+      .then((res) => res.json())
+      .then((res) => setPost(res));
+  };
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <div>{post.author.username}</div>
+      <div>{post.content}</div>
+      <Comments id={postId} />
+    </div>
+  );
 }
 
 export default PostDetail;
